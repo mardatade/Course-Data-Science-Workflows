31c424c9020f68e7f1c51bbd8913b0a68ca52050
calotra <65971852+calotra@users.noreply.github.com>
2020-11-11 15:49:39 +0100

    alternative way of installing R in binder

--- .binder/install.R
+++
@@ -1,1 +0,0 @@
-install.packages(c("stringi", "curl", "urltools", "httpcode", "plyr", "stringr", "pkgconfig", "rappdirs", "crul", "xml2", "oai", "tibble", "hoardr", "png"), repos = "http://cran.us.r-project.org")
